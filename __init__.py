2936e229f596ff67633b5916abe6fffc34e24ff0
ZenYy-7 <149468894+ZenYy-7@users.noreply.github.com>
2024-02-20 10:55:21 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, jsonify
+import json
+import sqlite3
 
 app = Flask(__name__) #creating flask app name
 
@@ -34,6 +36,17 @@ def submit_message():
     conn.close()
     
     return 'Message envoyé avec succès!'
+# Création d'une nouvelle route pour la lecture de la BDD
+@app.route('/lecture/')
+def ReadBDD():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM livres').fetchall()
+    conn.close()
 
+    # Convertit la liste de livre en un format JSON
+    json_posts = [{'id': post['id'], 'title': post['title'], 'content': post['auteur']} for post in posts]
+
+    # Renvoie la réponse JSON
+    return jsonify(posts=json_posts)
 if(__name__ == "__main__"):
     app.run()
