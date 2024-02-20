a0408a65f8304d24961a18ae03da863f144da887
ZenYy-7 <149468894+ZenYy-7@users.noreply.github.com>
2024-02-20 13:46:51 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -37,22 +37,5 @@ def ReadBDD():
 def contact_form():
     return render_template('contact.html')
 
-# Route pour gérer la soumission du formulaire de contact
-@app.route('/submit_contact_form', methods=['POST'])
-def submit_contact_form():
-    # Récupérer les données du formulaire soumis
-    name = request.form.get('name')
-    email = request.form.get('email')
-    message = request.form.get('message')
-
-    # Insérer les données dans la base de données SQLite
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO messages (name, email, message) VALUES (?, ?, ?)", (name, email, message))
-    conn.commit()
-    conn.close()
-
-    # Rediriger ou afficher un message de confirmation
-    return "Votre demande de contact a bien été envoyée."
 if(__name__ == "__main__"):
     app.run()
